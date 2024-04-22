27ce2ac5036ff0dfadbd0fc707670be9cfe6b307
Abdul Samad M J <samadmj4@gmail.com>
2024-04-22 17:57:59 +0400

    ts errors fixed

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,9 +1,5 @@
-import React from 'react'
-
 function About() {
-  return (
-    <div>About</div>
-  )
+  return <div>About</div>;
 }
 
-export default About
+export default About;
--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,9 +1,5 @@
-import React from 'react'
-
 function Contact() {
-  return (
-    <div>Contact</div>
-  )
+  return <div>Contact</div>;
 }
 
-export default Contact
+export default Contact;
