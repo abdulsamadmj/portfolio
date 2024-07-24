0e08de8f45585f8a0bc59aa5c4488bf7086dd740
Abdul Samad M J <samadmj4@gmail.com>
2024-07-24 20:20:31 +0400

    react error fixed

--- src/utils/ui/flip-words.tsx
+++ src/utils/ui/flip-words.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useCallback, useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { AnimatePresence, motion } from "framer-motion";
 import { cn } from "../cn";
 
